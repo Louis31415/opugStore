6d46edc82aee93cc59961c1f4e87e52271596c03
Louis Dorland <31774507+Louis31415@users.noreply.github.com>
2017-09-27 22:15:53 -0500

    Changes to support deployment to Apache2 with WSGI
    Changes to support deployment to Apache2 with WSGI
    
    Trouble shooting on a Raspberry Pi 3 running Apache2 and WSGI required these changes

--- Store/Store/wsgi.py
+++ Store/Store/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Store.settings")
+
+#Following line added to let Apache2 & WSGI find module named 'Store'
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
 application = get_wsgi_application()
