1a364a3cefa0cf869f968df786d1f8e2b1bff290
Louis Dorland <31774507+Louis31415@users.noreply.github.com>
2017-09-28 01:28:25 -0500

    Update allowed hosts
    Set allowed hosts to accept any host.

--- Store/Store/settings.py
+++ Store/Store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#jepura*udhfdh)3z8n174-03d)jss)4wzgz3=3oey=aogdyg0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [127.0.0.1]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
